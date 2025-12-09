f85447937c3a92333dc38e61cdf4e297efb29273
brokenspine27 <doris.ilabaca@inacapmail.cl>
2025-12-09 15:28:47 -0300

    mejora estilo splash

--- app/src/main/java/com/example/buttons_app/SplashActivity.kt
+++ app/src/main/java/com/example/buttons_app/SplashActivity.kt
@@ -8,12 +8,15 @@ import android.os.Looper
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.buttons_app.ui.theme.ButtonsappTheme
 
 @SuppressLint("CustomSplashScreen")
@@ -26,10 +29,19 @@ class SplashActivity : ComponentActivity() {
                     contentAlignment = Alignment.Center,
                     modifier = Modifier.fillMaxSize().background(MaterialTheme.colorScheme.background)
                 ) {
-                    Text(
-                        text = "Bienvenido a tu panel de control",
-                        color = MaterialTheme.colorScheme.onBackground
-                    )
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Text(
+                            text = "Bienvenido a tu panel de control",
+                            color = MaterialTheme.colorScheme.onBackground,
+                            fontSize = 24.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+                        Spacer(modifier = Modifier.height(16.dp))
+                        CircularProgressIndicator(color = MaterialTheme.colorScheme.secondary)
+                    }
                 }
             }
         }
